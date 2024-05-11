38c9fc82aa93a67e4d52e7f3abd6a653e613684d
wesclicdev <wesclicdev@gmail.com>
2024-05-11 11:42:42 +0700

    memperbaiki typo

--- stackusinglinkedlist.cpp
+++ stackusinglinkedlist.cpp
@@ -76,7 +76,7 @@ int main () {
                 stack.push(value);
                 break;
             case 2:
-                if (!stack/isEmpty()) {
+                if (!stack.isEmpty()) {
                     stack.pop();
                 }
                 else {
