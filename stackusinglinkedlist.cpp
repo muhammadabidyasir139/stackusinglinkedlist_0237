7d955275c55db8d4ec5031acf083dae33af5cebf
wesclicdev <wesclicdev@gmail.com>
2024-05-11 11:06:25 +0700

    membuat top operation

--- stackusinglinkedlist.cpp
+++ stackusinglinkedlist.cpp
@@ -36,4 +36,10 @@ class Stack {
             cout << "Popped value: " << top->data << endl;
             top = top->next;
         }
+
+        void peek() {
+            if (top == NULL) {
+                cout << "List is empty." << endl;
+            }
+        }
 };
